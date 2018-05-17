8e58a49f4e477c9b67299ebfc3550ee39889c3d0
vastopol <classicunix@gmail.com>
2018-05-17 15:31:01 -0700

    git script

--- scripts/git_set.sh
+++ scripts/git_set.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+GIT_NAME=vastopol
+GIT_EMAIL=classicunix@gmail.com
+
+echo "OLD:"
+git config -l
+rm ~/.gitconfig
+
 git config --global push.default simple
+git config --global user.name  $GIT_NAME
+git config --global user.email $GIT_EMAIL
+
+echo "NEW:"
+git config -l
