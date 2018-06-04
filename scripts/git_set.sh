c72d910f3b858550276ac7231fe36aed93fd48f8
vastopol <classicunix@gmail.com>
2018-06-03 21:17:00 -0700

    ed config

--- scripts/git_set.sh
+++ scripts/git_set.sh
@@ -2,6 +2,7 @@
 
 GIT_NAME=vastopol
 GIT_EMAIL=classicunix@gmail.com
+GIT_EDIT="nano"
 
 echo "OLD:"
 git config -l
@@ -10,6 +11,7 @@ rm ~/.gitconfig
 git config --global push.default simple
 git config --global user.name  $GIT_NAME
 git config --global user.email $GIT_EMAIL
+git config --global core.editor $GIT_EDIT
 
 echo "NEW:"
 git config -l
