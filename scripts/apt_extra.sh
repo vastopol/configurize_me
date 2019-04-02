68dd3f5c4a2b4d83fe18c84b6c485837b4ea3547
vastopol <classicunix@gmail.com>
2019-04-02 14:32:37 -0700

    pkg updt

--- scripts/apt_extra.sh
+++ scripts/apt_extra.sh
@@ -5,22 +5,28 @@
 
 # figure out how to do google chrome
 
+# DOWNLOADS PACKAGES
+
 # electrum 2.9.3
 sudo apt-get install python-qt4 python-pip
 sudo pip2 install https://download.electrum.org/2.9.3/Electrum-2.9.3.tar.gz
 
+# ADDING REPOSITORIES
+
 # atom text editor
 sudo add-apt-repository ppa:webupd8team/atom
-sudo apt-get update
-sudo apt install atom
 
 # ubuntu mate desktop
 sudo apt-add-repository ppa:ubuntu-mate-dev/ppa
 sudo apt-add-repository ppa:ubuntu-mate-dev/trusty-mate
-sudo apt-get install ubuntu-mate-core ubuntu-mate-desktop
 
-# java
+# java - Oracle java9
 sudo add-apt-repository ppa:webupd8team/java
+
+# update and install
 sudo apt-get update
+
+sudo apt install atom
+sudo apt-get install ubuntu-mate-core ubuntu-mate-desktop
 sudo apt-get install oracle-java9-installer
 
