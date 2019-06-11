3c032d046e0eb7098b606bdacc2a77f0e4911a6c
vastopol <classicunix@gmail.com>
2019-06-10 22:09:21 -0700

    edit brc script

--- scripts/bashrc_config.sh
+++ scripts/bashrc_config.sh
@@ -32,27 +32,6 @@ echo "# User functions:" >> $BRC
 #----------------------------------------
 
 echo '
-function git_srich()
-{
-    GIT_NAME=srich009
-    GIT_EMAIL=srich009@ucr.edu
-    GIT_PUSH=simple
-    GIT_EDIT=nano
-
-    rm ~/.gitconfig
-
-    git config --global user.name    $GIT_NAME
-    git config --global user.email   $GIT_EMAIL
-    git config --global push.default $GIT_PUSH
-    git config --global core.editor  $GIT_EDIT
-
-    echo ""
-    echo "NOW:"
-    git config -l
-    echo ""
-}' >> $BRC
-
-echo '
 function git_vast()
 {
     GIT_NAME=vastopol
